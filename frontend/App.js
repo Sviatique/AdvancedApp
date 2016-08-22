9c7e44af8777feaf73f2ff52342f489ab98ae2b9
LEVI9\s.kharchevnyi <s.kharchevnyi@levi9.com>
2016-08-22 14:47:29 +0300

    JsonP changed to CORS

--- frontend/App.js
+++ frontend/App.js
@@ -2,8 +2,9 @@
 const app = angular.module('App', []);
 
 app.service('AccountsManagement', function($http){
+    const url = 'http://localhost:1337/Account?callback=JSON_CALLBACK';
     this.getAccounts = () => {
-        return $http.jsonp('http://localhost:1337/Account?callback=JSON_CALLBACK').success((response) => {
+        return $http.get('http://localhost:1337/Account').then(response =>{
             return response;
         });
     };

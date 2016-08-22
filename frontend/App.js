a42c370d8bd7f9431688082a3195f14274127e78
LEVI9\s.kharchevnyi <s.kharchevnyi@levi9.com>
2016-08-22 14:47:54 +0300

    small changes

--- frontend/App.js
+++ frontend/App.js
@@ -2,9 +2,9 @@
 const app = angular.module('App', []);
 
 app.service('AccountsManagement', function($http){
-    const url = 'http://localhost:1337/Account?callback=JSON_CALLBACK';
+    const url = 'http://localhost:1337/Account';
     this.getAccounts = () => {
-        return $http.get('http://localhost:1337/Account').then(response =>{
+        return $http.get(url).then(response =>{
             return response;
         });
     };

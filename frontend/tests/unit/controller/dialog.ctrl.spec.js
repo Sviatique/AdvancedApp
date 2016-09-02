3a207bcb55780b55dcc015ac84a10d6883459933
LEVI9\s.kharchevnyi <s.kharchevnyi@levi9.com>
2016-09-02 14:41:27 +0300

    dialog ctrl tests

--- frontend/tests/unit/controller/dialog.ctrl.spec.js
+++ frontend/tests/unit/controller/dialog.ctrl.spec.js
@@ -1,25 +1,86 @@
 describe("dialogController", function() {
-    var dialogController;
+    var $controller;
     var accData;
     var mode;
+    var scope;
     
     beforeEach(module('app'));
     
-    beforeEach(inject(function($controller, $rootScope){
-        accData = {};
-        dialogController = $controller('dialogController', 
-           {
-                $scope: $rootScope,
-                accData: accData,
-                mode: mode
-           });
+    beforeEach(inject(function(_$controller_, $rootScope){
+        
+        $controller = _$controller_;
+        scope = $rootScope.$new();
+        accData = {
+            data: 'data'
+        };
+
+        
     }));
     
     it("exist", function() {
-        expect(dialogController == null).toBe(false);
+        var controller = $controller('dialogController', {
+            $scope: scope,
+            accData: accData,
+            mode: mode
+        });
+        expect(сontroller).toBeDefined();
+    });
+
+    
+    it("calls accountService method", function() {
+        var $q;
+        var controller;
+        
+        inject(function(_$q_){
+            $q = _$q_; 
+        });
+        
+        var dialogService = jasmine.createSpyObj('accountService', ['getAccountById']);
+        
+        accountService.getAccountById.and.returnValue($q.when({}));
+        
+        controller = $controller('dialogController', {
+            $scope: scope,
+            accData: accData,
+            mode: mode,
+            
+        });
+
+        expect(accountService.getAccountById).toHaveBeenCalled();
     });
     
-    it("calls service method", function(){
+    it("doesn't store any info on init", function() {
+        var controller;
         
+        controller = $controller('accountController as vm', {
+            $scope: scope
+        });
+
+        expect(scope.vm.accounts).toBeUndefined();
     });
+    
+    it("stores data from service after it is returned", function() {
+        var $q;
+        var controller;
+        
+        inject(function(_$q_){
+            $q = _$q_; 
+        });
+        
+        var accountService = jasmine.createSpyObj('accountService', ['getAccounts']);
+        
+        accountService.getAccounts.and.returnValue($q.when({
+            data: 'data'
+        }));
+        
+        controller = $controller('accountController as vm', {
+            accountService: accountService,
+            $scope: scope
+        });
+        
+        scope.$apply();
+        expect(scope.vm.accounts).toBe('data');
+    });
+    
+    
 });
